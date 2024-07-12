94de7d2c7fac46ecc8fbe58c42e15e8853a106c5
luan.mai <35148022+maituongluanvn@users.noreply.github.com>
2024-07-13 03:38:55 +0700

    eslint

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-import Image from "next/image";
-import { LinkWithChannel } from "../atoms/LinkWithChannel";
-import { ChannelSelect } from "./ChannelSelect";
-import { ChannelsListDocument, MenuGetBySlugDocument } from "@/gql/graphql";
-import { executeGraphQL } from "@/lib/graphql";
+import Link from 'next/link';
+import Image from 'next/image';
+import { LinkWithChannel } from '../atoms/LinkWithChannel';
+import { ChannelSelect } from './ChannelSelect';
+import { ChannelsListDocument, MenuGetBySlugDocument } from '@/gql/graphql';
+import { executeGraphQL } from '@/lib/graphql';
 
 export async function Footer({ channel }: { channel: string }) {
 	const footerLinks = await executeGraphQL(MenuGetBySlugDocument, {
-		variables: { slug: "footer", channel },
+		variables: { slug: 'footer', channel },
 		revalidate: 60 * 60 * 24,
 	});
 	const channels = process.env.SALEOR_APP_TOKEN
@@ -17,7 +17,7 @@ export async function Footer({ channel }: { channel: string }) {
 					// and use app token instead
 					Authorization: `Bearer ${process.env.SALEOR_APP_TOKEN}`,
 				},
-		  })
+			})
 		: null;
 	const currentYear = new Date().getFullYear();
 
@@ -25,12 +25,12 @@ export async function Footer({ channel }: { channel: string }) {
 		<footer className="border-neutral-300 bg-neutral-50">
 			<div className="mx-auto max-w-7xl px-4 lg:px-8">
 				<div className="grid grid-cols-3 gap-8 py-16">
-					{footerLinks.menu?.items?.map((item) => {
+					{footerLinks.menu?.items?.map(item => {
 						return (
 							<div key={item.id}>
 								<h3 className="text-sm font-semibold text-neutral-900">{item.name}</h3>
 								<ul className="mt-4 space-y-4 [&>li]:text-neutral-500">
-									{item.children?.map((child) => {
+									{item.children?.map(child => {
 										if (child.category) {
 											return (
 												<li key={child.id} className="text-sm">
@@ -84,12 +84,12 @@ export async function Footer({ channel }: { channel: string }) {
 				<div className="flex flex-col justify-between border-t border-neutral-200 py-10 sm:flex-row">
 					<p className="text-sm text-neutral-500">Copyright &copy; {currentYear} Your Store, Inc.</p>
 					<p className="flex gap-1 text-sm text-neutral-500">
-						Powered by{" "}
-						<Link target={"_blank"} href={"https://saleor.io/"}>
+						Powered by{' '}
+						<Link target={'_blank'} href={'https://saleor.io/'}>
 							Saleor
-						</Link>{" "}
-						<Link href={"https://github.com/saleor/saleor"} target={"_blank"} className={"opacity-30"}>
-							<Image alt="Saleor github repository" height={20} width={20} src={"/github-mark.svg"} />
+						</Link>{' '}
+						<Link href={'https://github.com/saleor/saleor'} target={'_blank'} className={'opacity-30'}>
+							<Image alt="Saleor github repository" height={20} width={20} src={'/github-mark.svg'} />
 						</Link>
 					</p>
 				</div>
