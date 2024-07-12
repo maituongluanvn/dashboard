c4071537eae35015b08ba342b6d820167b23a4c5
luan.mai <35148022+maituongluanvn@users.noreply.github.com>
2024-07-13 04:18:44 +0700

    fix image

--- src/zustand/productsStore.ts
+++ src/zustand/productsStore.ts
@@ -1,16 +1,36 @@
 import { create } from 'zustand';
 
 export const useProductStore = create(set => ({
-  product: [{
-    id: 'UHJvZHVjdDoxNTA=',
-    name: 'Saleor Mighty Mug',
-    slug: 'mighty-mug',
-    pricing: { priceRange: [Object] },
-    category: { id: 'Q2F0ZWdvcnk6NDE=', name: 'Homewares' },
-    thumbnail: {
-      url: 'https://drive.google.com/file/d/15Y37eVmloRpCo_lkI9DvguMyzSMhXJxm/view?usp=sharing',
-      alt: '',
-    },
-  }],
-  increasePopulation: () => set((state: { bears: number; }) => ({ bears: state.bears + 1 })),
+	product: [
+		{
+			id: 'UHJvZHVjdDoxNTA=',
+			name: 'Nước muối',
+			slug: 'nuoc-muoi',
+			pricing: {
+				priceRange: {
+					start: {
+						gross: {
+							amount: 54,
+							currency: 'VND',
+						},
+					},
+					stop: {
+						gross: {
+							amount: 90,
+							currency: 'VND',
+						},
+					},
+				},
+			},
+			category: {
+				id: 'Q2F0ZWdvcnk6NDE=',
+				name: 'Homewares',
+			},
+			thumbnail: {
+				url: 'https://3ymzvofxaogcuowg.public.blob.vercel-storage.com/444470698_1124776635298415_5589469441042249899_n-y4eron1o0LUCjyioNNDqi4p1dXkw0k.webp',
+				alt: '',
+			},
+		},
+	],
+	increasePopulation: () => set((state: { bears: number }) => ({ bears: state.bears + 1 })),
 }));
