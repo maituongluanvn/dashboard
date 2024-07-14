6d374919e1546e4c3284392abf1df5e0f804b626
luan.mai <35148022+maituongluanvn@users.noreply.github.com>
2024-07-14 14:04:15 +0700

    get json data

--- pages/api/product/[slug].ts
+++ pages/api/product/[slug].ts
@@ -1,6 +1,6 @@
-'use server'
-import { promises as fs } from 'fs';
+'use server';
 import { type NextApiRequest, type NextApiResponse } from 'next';
+import data from '../../../public/products.json';
 import type { IProduct } from '@definition/index';
 // type ResponseData = {
 // 	message?: string;
@@ -9,7 +9,7 @@ import type { IProduct } from '@definition/index';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	const { slug } = req.query;
-	const result = await fs.readFile(process.cwd() + '/products.json', 'utf8');
+	// const result = await fs.readFile(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products.json`, 'utf8');
 
 	// const result = await fetch(process.env.PRODUCT_JSON_URL as string, {
 	// 	headers: {
@@ -18,7 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	// 	},
 	// });
 	// const data: any = await result.json();
-	const { products }: { products: IProduct[] } = JSON.parse(result) as { products: IProduct[] } ;
+	const { products }: { products: IProduct[] } = data as unknown as { products: IProduct[] };
 	const product: IProduct | undefined = products.find((product: { slug: any }) => product.slug === slug);
 	if (!product) {
 		res.status(400).json({ message: `Not found product with slug: ${slug as string}` });
--- pages/api/product/index.ts
+++ pages/api/product/index.ts
@@ -1,15 +1,14 @@
 'use server';
-import { promises as fs } from 'fs';
 import { type NextApiRequest, type NextApiResponse } from 'next';
+import data from '../../../public/products.json';
 import type { IProduct } from '@definition/index';
-
 // type ResponseData = {
 // 	message?: string;
 // 	products?: IProduct[];
 // };
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
-    const result = await fs.readFile(process.cwd() + '/products.json', 'utf8');
+    // const result = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/products.json`);
 	// const result = await fetch(process.env.PRODUCT_JSON_URL as string, {
 	// 	headers: {
 	// 		'Content-Type': 'application/json',
@@ -17,7 +16,7 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
 	// 	},
 	// });
 
-	const { products }: { products: IProduct[] } = JSON.parse(result) as { products: IProduct[] } ;
+	const { products }: { products: IProduct[] } = data as unknown  as { products: IProduct[] } ;
 	if (!products) {
 		res.status(400).json({ message: `Not found products` });
 	}
