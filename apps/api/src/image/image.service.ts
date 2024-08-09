d62e114d22b6e8c0dc116cf93af98ed21036f46e
luan.mai <35148022+maituongluanvn@users.noreply.github.com>
2024-08-09 16:32:41 +0700

    [Enhance] Get service-account-keyfile secret

--- apps/api/src/image/image.service.ts
+++ apps/api/src/image/image.service.ts
@@ -1,28 +1,52 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Storage } from '@google-cloud/storage';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import { Readable } from 'stream';
-import * as path from 'path';
-import * as fs from 'fs'; // Thêm import module fs
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ImageService {
-	private readonly storage: Storage;
+	private storage: Storage | null = null; // Không dùng readonly ở đây
 	private readonly bucketName: string = 'hoangphuc'; // Tên bucket của bạn
-	private readonly keyFilePath: string = path.join(__dirname, '..', '..', 'service-account-keyfile.json'); // Đường dẫn đến tệp khóa
 
 	constructor() {
-		if (!fs.existsSync(this.keyFilePath)) {
-			// Kiểm tra sự tồn tại của tệp khóa
-			throw new HttpException('Service account key file does not exist', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		this.storage = new Storage({
-			keyFilename: this.keyFilePath, // Đường dẫn đến tệp khóa
+		this.initializeStorage().catch(err => {
+			throw new HttpException(
+				`Failed to initialize storage: ${err.message}`,
+				HttpStatus.INTERNAL_SERVER_ERROR,
+			);
 		});
 	}
 
+	private async initializeStorage(): Promise<void> {
+		const secretClient = new SecretManagerServiceClient();
+		const secretName = process.env.GCLOUD_SECRET_NAME;
+
+		if (!secretName) {
+			throw new HttpException('Secret name not provided', HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		try {
+			// Truy cập Secret
+			const [version] = await secretClient.accessSecretVersion({ name: secretName });
+			const keyFileContents = version.payload.data.toString();
+
+			// Khởi tạo Storage với tệp khóa JSON
+			this.storage = new Storage({ credentials: JSON.parse(keyFileContents) });
+		} catch (err) {
+			throw new HttpException(`Failed to access secret: ${err.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private checkStorageInitialized() {
+		if (!this.storage) {
+			throw new HttpException('Storage not initialized', HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 	async uploadFile(file: Express.Multer.File): Promise<string> {
+		this.checkStorageInitialized();
+
 		const bucket = this.storage.bucket(this.bucketName);
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 		const blob = bucket.file(`uploads/${uuidv4()}_${file.originalname}`); // Thư mục uploads
@@ -43,6 +67,8 @@ export class ImageService {
 	}
 
 	async fetchImage(filename: string): Promise<Buffer> {
+		this.checkStorageInitialized();
+
 		const bucket = this.storage.bucket(this.bucketName);
 		const file = bucket.file(`uploads/${filename}`); // Đảm bảo đường dẫn đúng
 
@@ -59,6 +85,8 @@ export class ImageService {
 	}
 
 	async deleteFile(filename: string): Promise<void> {
+		this.checkStorageInitialized();
+
 		const bucket = this.storage.bucket(this.bucketName);
 		const file = bucket.file(`uploads/${filename}`); // Đảm bảo đường dẫn đúng
 
