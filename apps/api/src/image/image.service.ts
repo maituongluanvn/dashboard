7da379a73652a1266380068ae8af166a0c91054c
luan.mai <35148022+maituongluanvn@users.noreply.github.com>
2024-08-12 00:21:09 +0700

    #32 convert image to webp

--- apps/api/src/image/image.service.ts
+++ apps/api/src/image/image.service.ts
@@ -1,3 +1,4 @@
+import * as sharp from 'sharp';
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Storage } from '@google-cloud/storage';
 import { Readable } from 'stream';
@@ -5,8 +6,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ImageService {
-	private storage: Storage | null = null; // Không dùng readonly ở đây
-	private readonly bucketName: string = 'hoangphuc'; // Tên bucket của bạn
+	private storage: Storage | null = null;
+	private readonly bucketName: string = 'hoangphuc';
 
 	constructor() {
 		this.initializeStorage().catch(err => {
@@ -28,7 +29,6 @@ export class ImageService {
 		}
 
 		try {
-			// Khởi tạo Storage với thông tin xác thực từ biến môi trường
 			this.storage = new Storage({
 				projectId,
 				credentials: {
@@ -54,16 +54,21 @@ export class ImageService {
 	async uploadFile(file: Express.Multer.File): Promise<string> {
 		this.checkStorageInitialized();
 
+		// Convert the image to WebP format using sharp
+		const convertedImage = await sharp(file.buffer)
+			.webp({ quality: 100 }) // Adjust the quality as needed
+			.toBuffer();
+
 		const bucket = this.storage.bucket(this.bucketName);
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-		const blob = bucket.file(`uploads/${uuidv4()}_${file.originalname}`); // Thư mục uploads
+		const webpFileName = `uploads/${uuidv4()}.webp`; // Save as WebP format
+		const blob = bucket.file(webpFileName);
 		const blobStream = blob.createWriteStream({
 			resumable: false,
-			contentType: file.mimetype,
+			contentType: 'image/webp', // Set the content type to WebP
 		});
 
 		return new Promise((resolve, reject) => {
-			Readable.from(file.buffer)
+			Readable.from(convertedImage)
 				.pipe(blobStream)
 				.on('error', err => reject(err))
 				.on('finish', () => {
@@ -77,7 +82,7 @@ export class ImageService {
 		this.checkStorageInitialized();
 
 		const bucket = this.storage.bucket(this.bucketName);
-		const file = bucket.file(`uploads/${filename}`); // Đảm bảo đường dẫn đúng
+		const file = bucket.file(`uploads/${filename}`);
 
 		return new Promise((resolve, reject) => {
 			file.download((err, data) => {
@@ -95,7 +100,7 @@ export class ImageService {
 		this.checkStorageInitialized();
 
 		const bucket = this.storage.bucket(this.bucketName);
-		const file = bucket.file(`uploads/${filename}`); // Đảm bảo đường dẫn đúng
+		const file = bucket.file(`uploads/${filename}`);
 
 		try {
 			await file.delete();
